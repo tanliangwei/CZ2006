ab7522e84706115fc836edcb6997b273b348d81e
Tan Liang Wei <liangweitan300895@gmail.com>
2018-03-16 20:32:18 +0800

    function signature for querying trash in database manager written

--- app/src/main/java/com/recyclingsg/app/DatabaseManager.java
+++ app/src/main/java/com/recyclingsg/app/DatabaseManager.java
@@ -1,8 +1,21 @@
 package com.recyclingsg.app;
 
+import java.util.ArrayList;
+
 /**
  * Created by tanliangwei on 16/3/18.
  */
 
 public class DatabaseManager {
+
+    //The attributes
+
+
+
+
+    //function for querying 
+    public ArrayList<TrashCollectionPoint> queryCollectionPoint(TrashPrices trashQuery){
+        ArrayList<TrashCollectionPoint> implementthisshit = new ArrayList<TrashCollectionPoint>();
+        return  implementthisshit;
+    }
 }
