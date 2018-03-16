e45681fd0073cc8601b32cc011e462876bff7566
rhowardliu <rhowardliu@gmail.com>
2018-03-16 23:41:38 +0800

    removed Node

--- app/src/main/java/com/recyclingsg/app/CollectionPointManager.java
+++ app/src/main/java/com/recyclingsg/app/CollectionPointManager.java
@@ -14,30 +14,27 @@ import java.util.List;
 public class CollectionPointManager {
 
     //to be removed
-    private List<Node> nodes;
 
 
     public CollectionPointManager(){
-        nodes = new ArrayList<Node>(){{
-            add (new Node("Jurong Recycling Station", "a1234", new LatLng(1.3343,(103.742))));
-            add (new Node("Pasir Ris Recyling Station", "b4321", new LatLng(1.3721, 103.9474)));
-            add (new Node("SouthWest Bound", "swb", new LatLng(1.22, 103.585)));
-            add (new Node("NorthEast Bound", "neb", new LatLng(1.472823, 104.087221)));
-
-
-        }};
+//        nodes = new ArrayList<Node>(){{
+//            add (new Node("Jurong Recycling Station", "a1234", new LatLng(1.3343,(103.742))));
+//            add (new Node("Pasir Ris Recyling Station", "b4321", new LatLng(1.3721, 103.9474)));
+//            add (new Node("SouthWest Bound", "swb", new LatLng(1.22, 103.585)));
+//            add (new Node("NorthEast Bound", "neb", new LatLng(1.472823, 104.087221)));
+//
+//
+//        }};
 
     }
 
-    public PrivateTrashCollectionPoint createCollectionPoint(String name, int zipCode, int openTime, int closeTime, TrashPrices[] trash, LatLng coordinates, int[]dayOpen, User owner, Date expiryDate, Date startDate)
-    {
-        PrivateTrashCollectionPoint newPrivateTrashCollectionPoint = new PrivateTrashCollectionPoint(name,zipCode,openTime,closeTime,trash,coordinates,dayOpen,owner,expiryDate,startDate);
-        return newPrivateTrashCollectionPoint;
-    }
+//    public PrivateTrashCollectionPoint createCollectionPoint(String name, int zipCode, int openTime, int closeTime, TrashPrices[] trash, LatLng coordinates, int[]dayOpen, User owner, Date expiryDate, Date startDate)
+//    {
+//        PrivateTrashCollectionPoint newPrivateTrashCollectionPoint = new PrivateTrashCollectionPoint(name,zipCode,openTime,closeTime,trash,coordinates,dayOpen,owner,expiryDate,startDate);
+//        return newPrivateTrashCollectionPoint;
+//    }
 
 
-    public List<Node> getNodes() {
-        return nodes;
-    }
+
 
 }
