674bc5b7303c261a668500d2f31256511286b28a
Tan Liang Wei <liangweitan300895@gmail.com>
2018-03-28 13:38:11 +0800

    successfully migrated trashdepostit manager to use trashInfo instead of trash pricese which is deprecated.

--- app/src/main/java/com/recyclingsg/app/TrashCollectionPointManager.java
+++ app/src/main/java/com/recyclingsg/app/TrashCollectionPointManager.java
@@ -51,9 +51,16 @@ public class TrashCollectionPointManager {
 
         Log.d(TAG, "createPrivateTrashCollectionPoint: creating..");
 
-        TrashInfo userTrashInfo = new TrashInfo();
-
         ArrayList<TrashInfo> trashInfoList = new ArrayList<TrashInfo>();
+        for(int i=0;i<trashTypes.size();i++){
+            if(!trashTypes.get(i).equalsIgnoreCase("Cash For Trash")) {
+                TrashInfo trashinfo = new TrashInfo(trashTypes.get(i));
+                trashInfoList.add(trashinfo);
+            }else if(trashTypes.get(i).equalsIgnoreCase("Cash For Trash")){
+                TrashInfo trashInfo = new TrashInfo(trashTypes.get(i),trashNames,units,trashPrices);
+                trashInfoList.add(trashInfo);
+            }
+        }
         int contact = Integer.parseInt(contactDetail);
         int openingTime = Integer.parseInt(openTime);
         int closingTime = Integer.parseInt(closeTime);
