1cd4999463fbb4587b467042ceab268a9dc49a67
Tan Liang Wei <liangweitan300895@gmail.com>
2018-04-04 10:58:49 +0800

    fixed searching issue

--- app/src/main/java/com/recyclingsg/app/FilterManager.java
+++ app/src/main/java/com/recyclingsg/app/FilterManager.java
@@ -39,20 +39,22 @@ public class FilterManager {
     }
 
     public void filterPrivateByCurrentDate(ArrayList<PrivateTrashCollectionPoint> beforeFilterTrashCollectionPoints){
-        Log.d(TAG, "filterByCurrentDate: "+ beforeFilterTrashCollectionPoints.get(0).getCollectionPointName()+beforeFilterTrashCollectionPoints.get(0).getCoordinate());
-        Date CurrentDate = new Date();
-        DateFormat sdf = new SimpleDateFormat("HHmm");
-        String CurrentDateString = sdf.format(CurrentDate);
-        Log.d(TAG, "filterByCurrentDate: checkingDateFormatString" + CurrentDateString);
-        int currentTimeInt = Integer.parseInt(CurrentDateString);
-        Log.d(TAG, "filterByCurrentDate: checkingCurrentTimeInt" + currentTimeInt);
-        for(int x = 0; x < beforeFilterTrashCollectionPoints.size(); x++){
-            if((beforeFilterTrashCollectionPoints.get(x).getOpenTimeInInt() >= currentTimeInt) || beforeFilterTrashCollectionPoints.get(x).getCloseTimeInInt() <= currentTimeInt){
-                closedTrashCollectionPoints.add(beforeFilterTrashCollectionPoints.get(x));
-                Log.d(TAG, "filterByCurrentDate: closed ones " + closedTrashCollectionPoints.get(x).getCollectionPointName());
-            } else {
-                openTrashCollectionPoints.add(beforeFilterTrashCollectionPoints.get(x));
-                Log.d(TAG, "filterByCurrentDate: opened ones" + openTrashCollectionPoints.get(0).getCollectionPointName());
+        if(beforeFilterTrashCollectionPoints.size()!=0) {
+            Log.d(TAG, "filterByCurrentDate: " + beforeFilterTrashCollectionPoints.get(0).getCollectionPointName() + beforeFilterTrashCollectionPoints.get(0).getCoordinate());
+            Date CurrentDate = new Date();
+            DateFormat sdf = new SimpleDateFormat("HHmm");
+            String CurrentDateString = sdf.format(CurrentDate);
+            Log.d(TAG, "filterByCurrentDate: checkingDateFormatString" + CurrentDateString);
+            int currentTimeInt = Integer.parseInt(CurrentDateString);
+            Log.d(TAG, "filterByCurrentDate: checkingCurrentTimeInt" + currentTimeInt);
+            for (int x = 0; x < beforeFilterTrashCollectionPoints.size(); x++) {
+                if ((beforeFilterTrashCollectionPoints.get(x).getOpenTimeInInt() >= currentTimeInt) || beforeFilterTrashCollectionPoints.get(x).getCloseTimeInInt() <= currentTimeInt) {
+                    closedTrashCollectionPoints.add(beforeFilterTrashCollectionPoints.get(x));
+                    Log.d(TAG, "filterByCurrentDate: closed ones " + closedTrashCollectionPoints.get(x).getCollectionPointName());
+                } else {
+                    openTrashCollectionPoints.add(beforeFilterTrashCollectionPoints.get(x));
+                    Log.d(TAG, "filterByCurrentDate: opened ones" + openTrashCollectionPoints.get(0).getCollectionPointName());
+                }
             }
         }
     }
