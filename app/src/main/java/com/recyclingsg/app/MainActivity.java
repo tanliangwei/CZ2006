1e11cc230130b30447b613f63a31834c67322f2e
David970804 <david19970804@gmail.com>
2018-04-13 12:26:21 +0800

    can redirect to target activity after successful login

--- app/src/main/java/com/recyclingsg/app/MainActivity.java
+++ app/src/main/java/com/recyclingsg/app/MainActivity.java
@@ -1,10 +1,8 @@
 package com.recyclingsg.app;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -451,7 +449,9 @@ public class MainActivity extends AppCompatActivity
             if(FacebookLogin.getLoginStatus()){
                 Intent intent = new Intent(MainActivity.this, FacebookLogin.class);
                 String message = "Please login in to Facebook first.";
+                String activity = "TrashPool";
                 intent.putExtra("message", message);
+                intent.putExtra("activity",activity);
                 startActivity(intent);
             }
             else {
@@ -462,7 +462,9 @@ public class MainActivity extends AppCompatActivity
             if(FacebookLogin.getLoginStatus()){
                 Intent intent = new Intent(MainActivity.this, FacebookLogin.class);
                 String message = "Please login in to Facebook first.";
+                String activity = "Statistics";
                 intent.putExtra("message", message);
+                intent.putExtra("activity",activity);
                 startActivity(intent);
             }
             else {
